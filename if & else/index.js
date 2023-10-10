2f3b6d9b9c87388fdb47da8b184b83d4966f4e23
JALDACO1 <144194369+JALDACO1@users.noreply.github.com>
2023-10-10 15:56:20 -0500

    Add files via upload

---
+++ if & else/index.js
@@ -0,0 +1,9 @@
+let edad = prompt("ingrese su edad",0);
+let esEstudiante = true;
+
+if (edad >= 18 && esEstudiante) {
+    console.log("Aplicas para descuento estudiantil.");
+} 
+ else {
+    console.log("No aplicas para el descuento.");
+}
